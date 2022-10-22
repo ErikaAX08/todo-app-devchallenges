a90ca73a525e94ddafbf22b725c9e7c8f2427f4c
Erika AX <eamastalxochimitl@gmail.com>
2022-10-21 23:41:14 -0500

    Added todos module

---
+++ src/modules/Todos.tsx
@@ -0,0 +1,19 @@
+import AddTodo from '../Components/AddTodo'
+import TodoItem from '../Components/TodoItem'
+
+function Todos() {
+  return (
+    <section>
+        <AddTodo/>
+
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+    </section>
+  )
+}
+
+export default Todos
