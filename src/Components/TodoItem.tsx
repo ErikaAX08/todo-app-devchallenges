48e18081ced01db5e58eb0532674f45e2e0f3d31
Erika AX <eamastalxochimitl@gmail.com>
2022-10-24 00:42:39 -0500

    Passing data and added functionality

--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -1,15 +1,31 @@
-import styles from './TodoItem.module.css'
+import type { RootState } from '../store/store';
+import { useSelector, useDispatch } from 'react-redux';
 
-function TodoItem() {
+import { setCompletedTodo, deleteTodo , Todo } from '../store/slices/todoSlice';
+import styles from './TodoItem.module.css'
+import { useEffect, useState } from 'react';
+
+function TodoItem({id, todo, completed}: Todo) {
+  
+  const [isCompleted, setIsCompleted] = useState<Boolean>(completed);
+  const tab = useSelector((state: RootState) => state.tab.tab);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    completed = isCompleted;
+    dispatch(setCompletedTodo({ id, todo, completed }))
+  }, [isCompleted]);
+
   return (
-    <label className={styles.container}>
-      <div className={`${styles.checkbox} ${styles.checked}`}>
+    <label className={`${styles.container} ${isCompleted && styles.todoCompleted}`}>
+      <div onClick={() => setIsCompleted((value) => !value)} className={`${styles.checkbox} ${isCompleted && styles.checked}`}>
         <img className={styles.iconDone} src='/done.svg' />
       </div>
-      Do challenges
-      <img onClick={() => console.log("Deleted")} className={styles.icon} src="/delete.svg" alt="Delete" />
+      <span onClick={() => setIsCompleted((value) => !value)}>{ todo }</span>
+      <img onClick={() => dispatch(deleteTodo({id, todo, completed}))} className={`${styles.icon} ${tab === 2 && styles.showIcon}`} src="/delete.svg" alt="Delete" />
     </label>
   );
 }
+
 
 export default TodoItem;
