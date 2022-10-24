4e74d7b5ba6e5403ac855322f0ba12feffaaeed7
Erika AX <eamastalxochimitl@gmail.com>
2022-10-24 00:45:30 -0500

    Added to-do item dynamically

--- src/modules/CompletedTodos.tsx
+++ src/modules/CompletedTodos.tsx
@@ -1,17 +1,26 @@
+import type { RootState } from '../store/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteAllTodos } from '../store/slices/todoSlice';
+
 import TodoItem from '../Components/TodoItem';
 import styles from './CompletedTodos.module.css';
 
 function CompletedTodos() {
+  const todos = useSelector((state: RootState) => state.todos.todos);
+  const dispatch = useDispatch();
+
+  const completedTodos = todos.filter((todo) => todo.completed === true); 
+
   return (
     <section className={styles.container}>
-        <TodoItem />
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
 
-        <button className={styles.button}>
+        {
+          completedTodos.map((todo) => (
+            <TodoItem key={todo.id} { ...todo } />
+          ))
+        }
+
+        <button className={styles.button} onClick={() => dispatch(deleteAllTodos())}>
             <img src='/delete-white.svg' className={styles.iconDelete} />
             delete all
         </button>
