906b95c7c1c02113d69534eb593055426ec8ccea
Erika AX <eamastalxochimitl@gmail.com>
2022-10-24 00:45:37 -0500

    Added to-do item dynamically

--- src/modules/Todos.tsx
+++ src/modules/Todos.tsx
@@ -1,17 +1,26 @@
+import { useEffect, useState } from 'react';
+import type { RootState } from '../store/store';
+import { useSelector } from 'react-redux';
+import { Todo } from '../store/slices/todoSlice';
+
 import AddTodo from '../Components/AddTodo'
 import TodoItem from '../Components/TodoItem'
 
 function Todos() {
+  const tab = useSelector((state: RootState) => state.tab.tab);
+  const todos = useSelector((state: RootState) => state.todos.todos);
+  const [selectedTodos, setSelectedTodos] = useState<Todo[]>(todos);
+
+  useEffect(() => tab === 0 ? setSelectedTodos(todos) : setSelectedTodos(todos.filter((todo) => todo.completed === false)), [tab, todos]);
+
   return (
     <section>
         <AddTodo/>
-
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
+        {
+           selectedTodos.map((todo) => (
+             <TodoItem  key={todo.id} {...todo} />
+           ))
+        }
     </section>
   )
 }
