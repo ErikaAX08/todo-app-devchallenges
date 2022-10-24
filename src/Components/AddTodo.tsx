e53833ebd63c9b399fe537e88846f42b72f119c2
Erika AX <eamastalxochimitl@gmail.com>
2022-10-24 00:40:36 -0500

    Adding functionality to the form

--- src/Components/AddTodo.tsx
+++ src/Components/AddTodo.tsx
@@ -1,10 +1,25 @@
+import { FormEvent, useState } from 'react';
+import { addTodo } from '../store/slices/todoSlice';
+import { useDispatch } from 'react-redux';
+
 import styles from './AddTodo.module.css';
+import randomNumer from '../lib/helpers/RandomNumber';
 
 function AddTodo() {
+  const [todo, setTodo] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    if (todo === '') return;
+    e.preventDefault();
+    dispatch(addTodo({ id: randomNumer(), todo, completed: false }));
+    setTodo('');
+  }
+
   return (
     <section className={styles.container}>
-        <form className={styles.form}>
-            <input className={styles.input} type="text" name="todo" id="todo" placeholder="add details" />
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <input className={styles.input} onChange={(e) => setTodo(e.target.value)} value={todo} type="text" name="todo" id="todo" placeholder="add details" />
             <button className={styles.button} type="submit">Add</button>  
         </form>
     </section>
