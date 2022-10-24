41e0cfb1cad0fdee068df04fc98fbf1dfa1bd623
Erika AX <eamastalxochimitl@gmail.com>
2022-10-24 00:43:45 -0500

    Toggle functionality added

--- src/Components/Toggle.tsx
+++ src/Components/Toggle.tsx
@@ -1,15 +1,22 @@
+import type { RootState } from '../store/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { setTab } from '../store/slices/tabSlice';
+
 import styles from './Toggle.module.css';
 
 function Toggle() {
+  const tab = useSelector((state: RootState) => state.tab.tab);
+  const dispatch = useDispatch();  
+
   return (
     <section className={styles.container}>
-        <div className={styles.toggleContainer}>
+        <div onClick={() => dispatch(setTab(0))} className={`${styles.toggleContainer} ${tab === 0 ? '' : styles.hidden }`}>
             <span className={styles.toggleText}>All</span>
         </div>
-        <div className={styles.toggleContainer}>
+        <div onClick={() => dispatch(setTab(1))} className={`${styles.toggleContainer} ${tab === 1 ? '' : styles.hidden }`}>
             <span className={styles.toggleText}>Active</span>
         </div>
-        <div className={styles.toggleContainer}>
+        <div onClick={() => dispatch(setTab(2))} className={`${styles.toggleContainer} ${tab === 2 ? '' : styles.hidden }`}>
             <span className={styles.toggleText}>Completed</span>
         </div>
     </section>
